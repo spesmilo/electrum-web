aded0b8df3f08ff0f9e5984f2b94cf9759a0a8ce
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-09-19 21:40:23 +0900

    Implement new design changes

--- js/electrum.js
+++ js/electrum.js
@@ -141,3 +141,22 @@ $(document).ready(function () {
 
 	checkPanelNavigation();
 });
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("headerWrapper");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
