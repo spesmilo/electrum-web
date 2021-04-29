6fafaeba5ce1f937f9c286c43fbd2ec139202fe0
ThomasV <thomasv@electrum.org>
2021-04-29 14:57:53 +0200

    update publish and deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,78 @@
 # cron job for the webserver
 set -ex
 cd "$(dirname "$0")"
+
+# fixme: we should not poll github
 git fetch github
+
 echo "latest commit $(git rev-parse github/master)"
-wget -q http://uploadserver/website.ThomasV.asc -O website.ThomasV.asc
+LOCAL_VERSION=$(git show master:version|jq '.version'|tr -d '"')
+VERSION=$(git show github/master:version|jq '.version'|tr -d '"')
+
+echo "local version: $LOCAL_VERSION "
+echo "github version: $VERSION "
+
+sftp -oBatchMode=no -b - pubwww@uploadserver << !
+   cd electrum-downloads-airlock
+   get website.ThomasV.asc
+   get website.SomberNight.asc
+   bye
+!
+
 git rev-parse github/master | gpg --verify website.ThomasV.asc -
-wget -q http://uploadserver/website.SomberNight.asc -O website.SomberNight.asc
 git rev-parse github/master | gpg --verify website.SomberNight.asc -
 
-echo "ok"
+echo "website signature verified"
+
+if [ $LOCAL_VERSION = $VERSION ];
+then
+    echo "updating website and exiting"
+    git pull github master
+    exit 0
+fi
+
+# 1. read from the airlock directory
+rm -rf airlock
+mkdir airlock
+cd airlock
+
+sftp -oBatchMode=no -b - pubwww@uploadserver << !
+   cd electrum-downloads-airlock
+   cd $VERSION
+   mget *
+   bye
+!
+
+# verify signatures
+tgz=Electrum-$VERSION.tar.gz
+appimage=electrum-$VERSION-x86_64.AppImage
+zip=Electrum-$VERSION.zip
+dmg=electrum-$VERSION.dmg
+win=electrum-$VERSION.exe
+win_setup=electrum-$VERSION-setup.exe
+win_portable=electrum-$VERSION-portable.exe
+
+for item in $tgz $appimage $zip $win $win_setup $win_portable
+do
+    gpg --verify $item $item.ThomasV.asc
+    gpg --verify $item $item.SomberNight.asc
+done
+
+# dmg
+gpg --verify $dmg $dmg.ThomasV.asc
+
+echo "verification passed"
+
+# publish files
+sftp -oBatchMode=no -b - pubwww@uploadserver << !
+   cd electrum-downloads
+   mkdir $VERSION
+   cd $VERSION
+   mput *
+   bye
+!
+
+# update website
 git pull github master
+
 # todo: clear cloudflare cache
--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
 # sign website commit and publish it
+# uploadserver neess to be defined in /etc/hosts
+
 user=$1
 if [ -z $user ]; then
-    echo "usage: publish.sh username"
+    echo "usage: publish.sh gpg_username"
     exit 1
 fi
 set -ex
@@ -9,7 +11,7 @@ cd "$(dirname "$0")"
 echo $(git rev-parse master)
 git rev-parse master | gpg --armor --detach > website.$user.asc
 sftp -oBatchMode=no -b - ${user,,}@uploadserver << !
-   cd electrum-downloads
+   cd electrum-downloads-airlock
    mput website.$user.asc
    bye
 !
