2710413764fbc5cd848ffc06b1dca74da7c77d8a
SomberNight <somber.night@protonmail.com>
2021-06-16 19:52:23 +0200

    publish.sh: set --local-user for gpg sign

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 # sign website commit and publish it
-# uploadserver neess to be defined in /etc/hosts
+# uploadserver needs to be defined in /etc/hosts
 
 user=$1
 if [ -z $user ]; then
@@ -9,7 +9,15 @@ fi
 set -ex
 cd "$(dirname "$0")"
 echo $(git rev-parse master)
-git rev-parse master | gpg --armor --detach > website.$user.asc
+
+PUBKEY=""
+if [ $user == "ThomasV" ]; then
+    PUBKEY="--local-user 6694D8DE7BE8EE5631BED9502BD5824B7F9470E6"
+elif [ $user == "sombernight_releasekey" ]; then
+    PUBKEY="--local-user 0EEDCFD5CAFB459067349B23CA9EEEC43DF911DC"
+fi
+git rev-parse master | gpg --sign --armor --detach $PUBKEY > website.$user.asc
+
 sftp -oBatchMode=no -b - ${user,,}@uploadserver << !
    cd electrum-downloads-airlock
    mput website.$user.asc
