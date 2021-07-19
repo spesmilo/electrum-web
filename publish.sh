da75b8d97ac40b515031bf527c1bc35c63d533d6
SomberNight <somber.night@protonmail.com>
2021-07-19 20:13:33 +0200

    publish.sh: distinguish gpg key name and ssh user name

--- publish.sh
+++ publish.sh
@@ -1,25 +1,28 @@
 # sign website commit and publish it
 # uploadserver needs to be defined in /etc/hosts
 
-user=$1
-if [ -z $user ]; then
-    echo "usage: publish.sh gpg_username"
+GPGUSER=$1
+if [ -z $GPGUSER ]; then
+    echo "usage: $0 gpg_username"
     exit 1
 fi
 set -ex
 cd "$(dirname "$0")"
 echo $(git rev-parse master)
 
+export SSHUSER="$GPGUSER"
 PUBKEY=""
-if [ $user == "ThomasV" ]; then
+if [ $GPGUSER == "ThomasV" ]; then
     PUBKEY="--local-user 6694D8DE7BE8EE5631BED9502BD5824B7F9470E6"
-elif [ $user == "sombernight_releasekey" ]; then
+    export SSHUSER=thomasv
+elif [ $GPGUSER == "sombernight_releasekey" ]; then
     PUBKEY="--local-user 0EEDCFD5CAFB459067349B23CA9EEEC43DF911DC"
+    export SSHUSER=sombernight
 fi
-git rev-parse master | gpg --sign --armor --detach $PUBKEY > website.$user.asc
+git rev-parse master | gpg --sign --armor --detach $PUBKEY > website.$GPGUSER.asc
 
-sftp -oBatchMode=no -b - ${user,,}@uploadserver << !
+sftp -oBatchMode=no -b - ${SSHUSER}@uploadserver << !
    cd electrum-downloads-airlock
-   mput website.$user.asc
+   mput website.$GPGUSER.asc
    bye
 !
