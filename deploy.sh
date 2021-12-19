969fa0ac406d9f3ef548c23c3fac7101add9ac7f
ThomasV <thomasv@electrum.org>
2021-12-19 17:17:20 +0100

    aggregated signatures:  - return early if there is no new commit  - for each new commit, download the airlock content, and create aggregated signature files  - the new commit is fetched before the airlock content is deployed, so that we can still    deploy a new version of this script, if the second part of the script crashes.    (this is not user visible because we still need to update the cloudflare cache)

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,14 @@ date -u
 
 # fixme: we should not poll github
 git fetch github
-echo "latest website commit $(git rev-parse github/master)"
+LOCAL_COMMIT="$(git rev-parse master)"
+REMOTE_COMMIT="$(git rev-parse github/master)"
+
+if [ $LOCAL_COMMIT = $REMOTE_COMMIT ];
+then
+    echo "no changes, exiting"
+    exit 0
+fi
 
 LOCAL_VERSION=$(cat version|jq '.version'|tr -d '"')
 VERSION=$(git show github/master:version|jq '.version'|tr -d '"')
@@ -28,17 +35,11 @@ git rev-parse github/master | gpg --no-default-keyring --keyring "$REPODIR/gpg/t
 git rev-parse github/master | gpg --no-default-keyring --keyring "$REPODIR/gpg/sombernight_releasekey.gpg" --verify website.sombernight_releasekey.asc -
 
 echo "website signature verified"
+# Update website immediately (in case the rest of the script crashes)
+git merge --ff-only FETCH_HEAD
 
-# Just updating website; no new release
-if [ $LOCAL_VERSION = $VERSION ];
-then
-    echo "updating website and exiting"
-    git merge --ff-only FETCH_HEAD
-    exit 0
-fi
 
-# As versions mismatched, there is a new release.
-# 1. read from the airlock directory
+# Read from the airlock directory
 rm -rf /tmp/airlock
 mkdir /tmp/airlock
 pushd /tmp/airlock
@@ -61,6 +62,8 @@ do
         # In case we upload any other file for whatever reason, both sigs are needed too.
         gpg --no-default-keyring --keyring "$REPODIR/gpg/thomasv.gpg" --verify "$item.ThomasV.asc" "$item"
         gpg --no-default-keyring --keyring "$REPODIR/gpg/sombernight_releasekey.gpg" --verify "$item.sombernight_releasekey.asc" "$item"
+        # create aggregated signature file
+        cat $item.*.asc > "$item.asc"
     fi
 done
 
@@ -69,14 +72,12 @@ echo "verification passed"
 # publish files
 sftp -oBatchMode=no -b - pubwww@uploadserver << !
    cd electrum-downloads
-   mkdir $VERSION
+   -mkdir $VERSION
    cd $VERSION
    mput *
    bye
 !
 
-# update website
 popd
-git merge --ff-only FETCH_HEAD
 
 # todo: clear cloudflare cache
